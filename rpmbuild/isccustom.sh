88bfc276c3f2918b5048b9cf04a983cef3ce7705
Repl-Sneha <sneha.hegde@replgroup.com>
2019-06-28 14:16:56 +0100

    SMB-3593-Carrier specific report

--- rpmbuild/isccustom.sh
+++ rpmbuild/isccustom.sh
@@ -13,19 +13,20 @@ fi
 echo "Starting postintall" > ${POSTINSTALL_LOG}
 date >> ${POSTINSTALL_LOG}
 
-#cd $LESDIR
-#echo "running mbuild" >> ${POSTINSTALL_LOG}
-#mbuild 2>&1 >> ${POSTINSTALL_LOG}
+cd $LESDIR
+echo "running mbuild" >> ${POSTINSTALL_LOG}
+mbuild 2>&1 >> ${POSTINSTALL_LOG}
 
-#echo "cd $LESDIR/db/data/load/base/bootstraponly" >> ${POSTINSTALL_LOG}
-#cd $LESDIR/db/data/load/base/bootstraponly
-#echo "running mload_all" >> ${POSTINSTALL_LOG}
-#mload_all 2>&1 >> ${POSTINSTALL_LOG}
+echo "cd $LESDIR/db/data/load/base/bootstraponly" >> ${POSTINSTALL_LOG}
+cd $LESDIR/db/data/load/base/bootstraponly
+echo "running mload_all" >> ${POSTINSTALL_LOG}
+mload_all 2>&1 >> ${POSTINSTALL_LOG}
 
-#echo "cd $LESDIR/db/data/load/base/safetoload" >> ${POSTINSTALL_LOG}
-#cd $LESDIR/db/data/load/base/safetoload
-#echo "running mload_all" >> ${POSTINSTALL_LOG}
-#mload_all 2>&1 >> ${POSTINSTALL_LOG}
+echo "cd $LESDIR/db/data/load/base/safetoload" >> ${POSTINSTALL_LOG}
+cd $LESDIR/db/data/load/base/safetoload
+echo "running mload_all" >> ${POSTINSTALL_LOG}
+mload_all 2>&1 >> ${POSTINSTALL_LOG}
+
 
 date >> ${POSTINSTALL_LOG}
 echo "Done" >> ${POSTINSTALL_LOG}
